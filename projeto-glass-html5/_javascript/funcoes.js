f747b69012b4a63537fe97ecbde0efaec19e82e8
orlandoeal <orlando.eal_@hotmail.com>
2016-07-31 02:02:16 -0300

    HTML5 / CSS3 / JavaScript

---
+++ projeto-glass-html5/_javascript/funcoes.js
@@ -0,0 +1,9 @@
+function mudaFoto(foto) {
+		document.getElementById("icone").src=foto;
+	}
+
+	function calcTotal () {
+		 var qtd = parseInt(document.getElementById("cQtd").value);
+		 tot = qtd * 1500;
+		 document.getElementById("cPreco").value = tot;
+	}
